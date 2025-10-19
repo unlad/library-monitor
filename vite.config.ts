a3ea4ce39e69ffb8482f29b5ddb3622b36e19855
xi-pec <xepicscripter@gmail.com>
2025-10-19 22:39:02 +0800

    fix: set conditional base for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
+const repo = "library-monitor"
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-});
+  base: command === 'build' ? `/${repo}/` : '/', //
+}));
