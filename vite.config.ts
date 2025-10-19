3c49acaf95bd1cb5f1b30dcf2be497e544c08cd6
xi-pec <xepicscripter@gmail.com>
2025-10-19 22:47:17 +0800

    fix: tweak react router for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 const repo = "library-monitor"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  base: command === 'build' ? `/${repo}/` : '/', //
+  base: command === 'build' ? `/${repo}/` : '/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 }));
